1808d835d90da4f8b17e102ae958230582919485
Furqan Ahmed <furqan.ah224@gmail.com>
2025-05-21 19:48:17 -0400

    Update Part A.java

--- Q4/Part A.java
+++ Q4/Part A.java
@@ -1,1 +1,14 @@
+    import java.util.Random;
+    public class SumOrSameGame{
+        private int[][] puzzle;
 
+        public SumOrSameGame(int numRows, int numCols){
+            puzzle = new int[numRows][numCols];
+            Random r = new Random();
+
+            for(int i = 0; i < numRows; i++){
+                for(int j = 0; j < numCols; j++){
+                    puzzle[i][j] = rand.nextInt(9) + 1;
+                }
+            }
+        }
