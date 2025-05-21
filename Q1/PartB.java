e5ee21acf3b3ad8062584c001fcc2b6659b3635c
Furqan Ahmed <furqan.ah224@gmail.com>
2025-05-21 16:54:40 -0400

    Update PartB.java

--- Q1/PartB.java
+++ Q1/PartB.java
@@ -3,7 +3,7 @@
         for(int i = startHour; i <= endHour; i++ ){
             if(maxDogs == walkDogs(i){
                 amount += 5 * maxDogs + 3;
-            if(dogsWalked == maxDogs || (hour >=9 && hour <= 17)){
+            if(dogsWalked == maxDogs || (i >=9 && i <= 17)){
                 amount +=3;
             }
         
