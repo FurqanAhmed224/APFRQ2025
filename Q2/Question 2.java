20a76f657c2d3b02ce08934b2f4f5a2dc96d8ba1
Furqan Ahmed <furqan.ah224@gmail.com>
2025-05-21 17:30:03 -0400

    Update Question 2.java

--- Q2/Question 2.java
+++ Q2/Question 2.java
@@ -1,1 +1,28 @@
+public class SignedText{
+        private String first;
+        private String last;
 
+        public SignedText(String firstName, String lastName){
+            this.first = firstName;
+            this.last = lastName;
+        }
+
+        public String getSignature(){
+            if(first.length()==0){
+                return last;
+            }
+            return first.charAt(0) + "-" + last;
+        }
+
+        public String addSignature(String texts){
+            String s = getSignature();
+            int index = texts.indexOf(s);
+            if(index == 0){
+                return texts.substring(s.length()) + s;
+            } else if(index == texts.length()-s.length()){
+                return texts;
+            } else{
+                return text + s;
+            }
+        }
+    }
