6a26632051d7700e30e3314edbdf917cfe840c0b
Furqan Ahmed <furqan.ah224@gmail.com>
2025-05-21 16:53:59 -0400

    Update PartB.java

--- Q1/PartB.java
+++ Q1/PartB.java
@@ -1,1 +1,13 @@
-
+   public int dogWalkShift(int startHour, endHour){
+        int amount = 0;
+        for(int i = startHour; i <= endHour; i++ ){
+            if(maxDogs == walkDogs(i){
+                amount += 5 * maxDogs + 3;
+            if(dogsWalked == maxDogs || (hour >=9 && hour <= 17)){
+                amount +=3;
+            }
+        
+            
+        }
+        return amount;
+    }
