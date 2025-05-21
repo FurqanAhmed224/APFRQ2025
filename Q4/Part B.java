a4a42780c7786687720fc4bded96e51338f9e01c
Furqan Ahmed <furqan.ah224@gmail.com>
2025-05-21 19:48:27 -0400

    Update Part B.java

--- Q4/Part B.java
+++ Q4/Part B.java
@@ -1,1 +1,20 @@
+public boolean clearPair(int row, int col){
+            int target = puzzle[row][col];
 
+            if(target == 0){
+                return false;
+            }
+
+            for(int i = row; i < puzzle.lengthl i++){
+                for(int j = 0; j < puzzle[i].length; j++){
+                    if((puzzle[i][j] == target || puzzle[i][j] + target == 10) && puzzle[i][j] != 0){
+                        puzzle[row][col] = 0;
+                        puzzle[i][j] = 0;
+                        return true;    
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
