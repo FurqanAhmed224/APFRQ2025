53ca61babdbe28ccff947bd60abdd51720aa9e0b
Furqan Ahmed <furqan.ah224@gmail.com>
2025-05-21 16:53:42 -0400

    Update PartA.java

--- Q1/PartA.java
+++ Q1/PartA.java
@@ -1,1 +1,11 @@
 
+
+Public int walkDogs(int hour){
+        int totalDogs = company.numAvailableDogs(hour);
+        int dogsWalked = totalDogs;
+        if(totalDogs >maxDogs){
+            dogsWalked == maxDogs;
+        }
+        company.updateDogs(hour,dogsWalked);
+        return dogsWalked;
+    }
